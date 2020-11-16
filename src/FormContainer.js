8c373997808e2075e0f54ac9a1d8fbde12916216
WhitestRabbit <stefanosmixalas@gmail.com>
2020-11-16 11:03:15 +0200

    hook fix

--- src/FormContainer.js
+++ src/FormContainer.js
@@ -11,16 +11,13 @@ const FormContainer = (props) => {
     gender: "",
     favColor: ""
   });
-
+  //it is vital to remember that the function returned by useState, that we easily named as setState, does not work exactly like the setState we learned from class component logic. This function essentially overwrites the entire state of the component with the object we pass in the function and can lead to problems when other properties of the state are missing. Therefore, whenever we call it here, it is smart to include the previous snapshot of the state with spread syntax, and then the updated property.
+  //In order to overcome the need to spread, we can define different states, maybe even one for each initial state property by using useState every time, by passing in the current state property, returning the snapshot of the current state property and the function that alters it!
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
     type === "checkbox"
-      ? setState({
-          [name]: checked
-        })
-      : setState({
-          [name]: value
-        });
+      ? setState({ ...state, [name]: checked })
+      : setState({ ...state, [name]: value });
   };
 
   const handleSubmit = () => {
